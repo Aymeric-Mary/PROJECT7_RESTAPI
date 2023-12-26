9da1c74e8a1fc5cd79c19bf8cff96f064b0c1125
Aymeric Mary <aymeric.mary.dev@gmail.com>
2023-12-26 10:57:14 +0100

    Fix

--- src/main/java/com/nnk/springboot/domain/Login.java
+++ src/main/java/com/nnk/springboot/domain/Login.java
@@ -8,11 +8,9 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Login {
-    @NotBlank(message = "Username is mandatory")
     @Column(name = "username", length = 125)
     private String username;
 
-    @NotBlank(message = "Password is mandatory")
     @Column(name = "password", length = 125)
     private String password;
 }
