24b6a9b1807db619a601816e8e5d544274b1697c
Aymeric Mary <aymeric.mary.dev@gmail.com>
2023-12-26 10:55:48 +0100

    Fix

--- src/main/java/com/nnk/springboot/controllers/LoginController.java
+++ src/main/java/com/nnk/springboot/controllers/LoginController.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -15,10 +16,13 @@ public class LoginController {
     private final UserService userService;
 
     @GetMapping("login")
-    public ModelAndView login() {
+    public ModelAndView login(@RequestParam(value = "error", required = false) boolean error) {
         ModelAndView mav = new ModelAndView();
         mav.addObject(new Login());
         mav.setViewName("login/form");
+        if(error) {
+            mav.addObject("error", "Invalid username or password!");
+        }
         return mav;
     }
 
